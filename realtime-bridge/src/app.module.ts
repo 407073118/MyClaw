74e0b18881101fb52fd44599232063335ca5eac5
407073118 <407073118@qq.com>
2026-05-18 12:58:53 +0800

    feat(realtime-bridge): 回发桌面端回复到钉钉

--- realtime-bridge/src/app.module.ts
+++ realtime-bridge/src/app.module.ts
@@ -5,9 +5,10 @@ import { ConversationModule } from "./modules/conversation/conversation.module";
 import { DeliveryModule } from "./modules/delivery/delivery.module";
 import { HealthModule } from "./modules/health/health.module";
 import { IngressModule } from "./modules/ingress/ingress.module";
+import { OutboundModule } from "./modules/outbound/outbound.module";
 import { RoutingModule } from "./modules/routing/routing.module";
 
 @Module({
-  imports: [HealthModule, IngressModule, DesktopWsModule, ConversationModule, RoutingModule, DeliveryModule],
+  imports: [HealthModule, IngressModule, DesktopWsModule, ConversationModule, RoutingModule, DeliveryModule, OutboundModule],
 })
 export class AppModule {}
